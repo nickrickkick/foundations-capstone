72bc71ce5290a1d565e74bbe5b53c6ba29c3cdd2
Michael Nowlin <michaelnowlin5@gmail.com>
2022-05-19 20:40:56 -0600

    updated html and created basic server backend. now will focus on main.js to link the front-end with back-end.

---
+++ server/controller.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+const {CONNECTION_STRING} = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+        drop table if exists users;
+        drop table if exists cutting_instructions;
+
+        create table users (
+            user_id serial primary key, 
+            username varchar,
+            name varchar,
+            password varchar(40),
+            phone_number int
+        );
+
+        create table cutting_instructions (
+            cutting_id serial primary key,
+            cutting_style varchar,
+            type_of_beef varchar,
+            price integer,
+            bought boolean,
+            user_id INTEGER REFERENCES users(user_id)
+        );
+
+        insert into users (username, name, password, phone_number)
+        values ('phonyMe', 'Kyle', 'badpassword', 8018224311),
+        ('clarkdark', 'phil', 'password', 8014356671),
+        ('darkknight', 'chad', 'stupidword', 8014551637).
+        ('fartman', 'tanner', 'badword', 8015554617),
+        ('firesword', 'franny', 'weakword', 8016667866)
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    },
+    getUsers: (req, res) => {
+        sequelize.query(`select name from users`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    },
+    createCutting: (req, res) => {
+        sequelize.query(`INSERT INTO cutting_instructions(cutting_style, type_of_beef, price, bought, user_id)
+        VALUES ('${req.body.cutting}', ${req.body.beef}, ${req.body.price}, ${req.body.bought}, ${req.body.userId});`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    },
+    getCutting: (req, res) => {
+        sequelize.query(`select cutting_id, cutting_style, type_of_beef, price, bought, users.user_id, name, phone_number
+        from cutting_instructions cutting_id
+        join users name on cutting_instructions.user_id = users.user_id;
+        join users phone_number on cutting_instructions.user_id = users.user_id;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    },
+    deleteCutting: (req, res) => {
+        sequelize.query(`DELETE FROM cutting_instructions WHERE cutting_id = ${req.params.id};`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {SERVER_PORT} = process.env
+const {seed, getUsers, createCutting, getCutting, deleteCutting} = require('./controller.js')
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/seed', seed)
+
+app.get('/users', getUsers)
+
+app.post('/cutting', createCutting)
+app.get('/cutting', getCutting)
+app.delete('/cutting/:id', deleteCutting)
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
